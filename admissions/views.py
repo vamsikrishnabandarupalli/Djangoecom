e2916e8e4f1fe8327965dd90f01eea737d0c962d
Vamsi Krishna Bandarupalli <154987040+vamsikrishnabandarupalli@users.noreply.github.com>
2024-10-18 22:30:29 +0530

    Update views.py

--- admissions/views.py
+++ admissions/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def fun(req):
-    return HttpResponse("This is function")
+    return HttpResponse("Every one is attended to the class.")
 
 def index(request):
     return render(request, 'index.html')
